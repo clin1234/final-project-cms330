eca27d000e1e64e37e1b3d594a6addfda18088cf
Charlie Lin <clin@rollins.edu>
2021-04-29 15:51:47 -0400

    WIP

--- thread.c
+++ thread.c
@@ -1,164 +1,208 @@
 // fyi: Latin comments are sprinkled everywhere.
 
+#include <assert.h>
+#include <iso646.h>
+#include <limits.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <string.h>
-#include <iso646.h>
+#include <time.h>
 
-enum { N = 50 }; // Maximum number of processes
-                 // Maximus numerus processorum
+short N; // Maximum number of processes aut maximus numerus processorum
+int highest_process_id = 0; // Highest process ID currently allocated
+const int invalid_pid = -1;
+short i; // Index counter
 
-struct node
-{
-   short pcb_index;
-  struct node* child;
+struct node {
+  short pcb_index;
+  struct node *child;
 };
+// Two structures: one containing a linked list and another a bunch of ints
 
-/*
-* Standard operations on linked lists
-* Normales operationes ligatis tabulatis
-*/
-static struct node* traverse(struct node* head)
-{
-  struct node* tmp = head;
-  while (tmp->child not_eq NULL)
-  {
-    tmp = tmp->child;
-  }
-  return tmp;
-}
-
-void append(struct node* head, struct node* data)
-{
-  struct node* tmp = malloc(sizeof(struct node));
-  tmp = traverse(head);
-  tmp->child = data;
-}
-
-struct pcb1_s
-{
-   short parent;
-  struct node* children;
-} pcb1_def = {0, NULL};
+struct pcb1_s {
+  short parent;
+  struct node *children;
+} pcb1_def = {invalid_pid, NULL};
 
 typedef struct pcb1_s pcb1;
 
-struct pcb2_s
-{
-   short parent, first_child, younger_sibling, older_sibling;
-} pcb2_def = {-1,-1,-1,-1}; // Invalid values
+struct pcb2_s {
+  short parent, first_child, younger_sibling, older_sibling;
+} pcb2_def = {invalid_pid, invalid_pid, invalid_pid, invalid_pid};
 
 typedef struct pcb2_s pcb2;
 
-pcb1 processes_l[N] = {{0,NULL}};
-pcb2 processes_a[N] = {{-1,-1,-1,-1}};
-
-
 /*
 
-Function suffixed with _l pertain to the linked list implementation, and 
-those with _a pertain to the integers implementation.
+Function suffixed with _l pertain to the linked list implementation, and those
+with _a pertain to the integers implementation.
 
 */
 
-// Odio hoc frustrum scribere... 
-void create_l( short p)
-{
-  struct node* tmp = malloc(sizeof(struct node));
-  for ( short i = 1; i < N; i++)
-  {
-    if (processes_l[i].children == NULL)
-    {
-      processes_l[i].parent = p;
+// Odio hoc frustrum scribere...
+void create_l(pcb1 *arr, short parent_process_id) {
+  struct node *tmp = malloc(sizeof(struct node));
+  for (short i = 1; i < N; i++) {
+    if (arr[i].parent == invalid_pid) {
+      highest_process_id = i;
+      arr[i].parent = parent_process_id;
       tmp->pcb_index = i;
-      //processes_l[i].children = NULL;
-      //processes_l[i].children->pcb_index = i;
-      //tmp = procceses_l[i];
-      append(processes_l[p].children, tmp);
+      // Appending a node to end of linked list
+      if (arr[parent_process_id].children == NULL) {
+        arr[parent_process_id].children = tmp;
+        arr[parent_process_id].children->child = NULL;
+      }
+      else{
+        struct node *p = arr[parent_process_id].children;
+        while (p->child != NULL) p = p->child;
+        p->child = tmp;
+        tmp->child = NULL;
+      }
       break;
     }
   }
 }
 
-void destroy_l( short p)
-{
-  struct node* tmp = processes_l[p].children;
-  while (tmp not_eq NULL)
-  {
-    destroy_l(tmp->child->pcb_index);
-    //free(processes_l[tmp->pcb_index]);
-    //free();
+void destroy_l(pcb1 *arr, short p) {
+  struct node *tmp = arr[p].children;
+  if (tmp == NULL) return;
+  while (tmp not_eq NULL) {
+    destroy_l(arr, tmp->pcb_index);
+    // free(NULL);
+    // free(arr[tmp->pcb_index]);
+    // free();
   }
+  arr[p].children = NULL;
 }
 
+// Sed hoc frustrum magis odio.
 // p: parent process
-void create_a( short p)
-{
-    if (processes_a[p].first_child == -1)
-    {
-      for ( short i = 1; i < N; i++)
-      {
-        if (processes_a[i].parent == -1)
-        {
-          processes_a[p].first_child = i;
-          processes_a[i].parent = p;
-          break;
-        }
+void create_a(pcb2 *arr, short p) {
+  if (arr[p].first_child == invalid_pid) {
+    for (short i = 1; i < N; i++) {
+      // Find first PCB that does not contain a valid parent
+      if (arr[i].parent == invalid_pid) {
+        arr[p].first_child = i;
+        arr[i].parent = p;
+        highest_process_id = i;
+        break;
       }
     }
-    else
-    {
-      for ( short i = 1; i < N; i++)
-      {
-        if (processes_a[i].parent == -1)
-        {
-          processes_a[i].parent = p;
-           short tmp = processes_a[p].first_child,
-              cur = processes_a[tmp].younger_sibling;
-          while (cur not_eq -1)
-            cur = processes_a[cur].younger_sibling;
-          processes_a[cur].younger_sibling = i;
-          processes_a[i].older_sibling = processes_a[cur].younger_sibling;
-          break;
-        }
+  } else {
+    for (short i = 1; i < N; i++) {
+      if (arr[i].parent == invalid_pid) {
+        arr[i].parent = p;
+        short tmp = arr[p].first_child, cur = arr[tmp].younger_sibling;
+        while (cur not_eq -1)
+          cur = arr[cur].younger_sibling;
+        arr[cur].younger_sibling = i;
+        arr[i].older_sibling = arr[cur].younger_sibling;
+        highest_process_id = i;
+        break;
       }
     }
+  }
 }
 
-void destroy_a( short p)
-{
-  for ( short i = 0; i < N; i++)
+void destroy_a(pcb2 *arr, short p) {
+  if (arr[p].first_child not_eq invalid_pid) {
+    arr[p].first_child = invalid_pid;
+  }
+}
+
+void print_stats_l(pcb1 *arr) {
+  for (i = 0; i < N; i++) {
+    printf("Process %d: parent %d\n", i, arr[i].parent);
+    printf("\tChildren: ");
+    for (struct node *t = arr[i].children; t != NULL; t = t->child)
+      printf("%d ", t->pcb_index);
+    puts("");
+  }
+}
+
+void print_stats_a(pcb2 *arr) {
+  for (i = 0; i < N; i++) {
+    printf("Process %d: parent %d\n", i, arr[i].parent);
+    if (arr[i].first_child != invalid_pid)
+      printf("\tFirst child: %d\n", arr[i].first_child);
+    if (arr[i].younger_sibling != invalid_pid)
+      printf("\tYounger sibling: %d\n", arr[i].younger_sibling);
+    if (arr[i].older_sibling != invalid_pid)
+      printf("\tOlder sibling: %d\n", arr[i].older_sibling);
+    puts("");
+  }
+}
+
+int main(int b, char **argv) {
+
+  if (b == 2 && atoi(argv[1]) > 0 && atoi(argv[1]) <= SHRT_MAX) {
+    N = atoi(argv[1]);
+  } else {
+    puts("This program only takes one argument: a positive number to"
+         " represent the maximum number of processes.");
+    exit(EXIT_FAILURE);
+  }
+
+  pcb1 *processes_l = malloc(N * sizeof(pcb1));
+  pcb2 *processes_a = malloc(N * sizeof(pcb2));
+
+  unsigned short i;
+  for (i = 0; i < N; i++) {
+    processes_l[i] = pcb1_def;
     processes_a[i] = pcb2_def;
-}
+  }
+  // Handle pcb[0] at start
+  processes_l[0].parent = processes_a[0].parent = 0;
 
-int main()
-{
-  puts("Linked list implementation");
-  time_t start = time(NULL);
-  for ( short i = 0; i < 5; i++)
-  {
-    for ( short j = 0; j < 10; j++)
-    {
-      create_l(i);
-    }
-  }
-  for ( short i = 0; i < 5; i++) destroy_l(i);
-  time_t end = time(NULL);
-  
-  printf("Elapsed time: %f seconds\n", difftime(start, end));
-  
-  puts("Integers implementation");
+  time_t start, end;
+  int random_number = 0;
+
+  puts("Linked list implementation\n");
+
+  // Randomness of rand() leaves much to be desired.
+/*
   start = time(NULL);
-  for ( short i = 0; i < 5; i++)
-  {
-    for ( short j = 0; j < 10; j++)
-    {
-      create_a(i);
-    }
+  srand(time(NULL));
+  int random_number = 0;
+  for (i = 0; i < N; i++) {
+    create_l(processes_l, random_number);
+    random_number = rand() % highest_process_id;
   }
-  for ( short i = 0; i < 5; i++) destroy_a(i);
+
+  print_stats_l(processes_l);
+
+  puts("Creation complete\n");
+  puts("");
+  for (i = 0; i < N; i++)
+    destroy_l(processes_l, random_number);
   end = time(NULL);
-  
-  printf("Elapsed time: %f seconds\n", difftime(start, end));
+
+  double linked_elapsed = difftime(end, start) * 1000;
+  printf("Elapsed time: %.1f ms\n", linked_elapsed);
+  */
+
+  // Reset for other implementation.
+  highest_process_id = 0;
+
+  puts("Integers implementation\n");
+  start = time(NULL);
+  srand(time(NULL));
+  random_number = 0;
+  for (i = 0; i < N; i++) {
+    create_a(processes_a, random_number);
+    random_number = rand() % highest_process_id;
+  }
+  print_stats_a(processes_a);
+  for (i = 0; i < N; i++)
+    destroy_a(processes_a, random_number);
+  end = time(NULL);
+
+  double ints_elapsed = difftime(end, start) * 1000;
+  printf("Elapsed time: %.1f ms\n", ints_elapsed);
+/*
+  printf("\nFor %d elements, struct of ints is ", N);
+  printf(ints_elapsed > linked_elapsed ? "slower" : "faster");
+  printf(" than linked list by %.1f ms.\n",
+         fabs(ints_elapsed - linked_elapsed));*/
 }
